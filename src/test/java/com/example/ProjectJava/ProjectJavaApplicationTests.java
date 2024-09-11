5a06b07238dd84a59d476c40b50e49a7896932c8
MANH NGUYEN DUC <135792849+manhwork@users.noreply.github.com>
2024-09-11 22:40:52 +0700

    Initial commit

---
+++ src/test/java/com/example/ProjectJava/ProjectJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ProjectJava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
